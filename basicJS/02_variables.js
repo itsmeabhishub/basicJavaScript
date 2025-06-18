0efadae83bb8716a31428ab032cc16eeb9faac89
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-18 21:44:55 +0000

    variables in JS

---
+++ basicJS/02_variables.js
@@ -0,0 +1,15 @@
+const accountNumber= 123456 // not changes
+
+let firstName= 'Abhishek'
+
+let secondName = 'Anand'
+var accountType = 'saving'
+
+currentAddres= 'jamui'
+
+
+console.log(firstName+ ' ' + secondName , accountNumber, accountType, currentAddres)
+
+currentAddres ='Kolkata'
+
+console.log(firstName+ ' ' + secondName , accountNumber, accountType, currentAddres)
