3b201ef87e1c0b1939ae5ccf80dd012a6d881d68
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-18 22:05:32 +0000

    conversion operation

---
+++ basicJS/03_conversionOpertaion.js
@@ -0,0 +1,36 @@
+// let score = '33'
+// let score = null
+let score = true
+
+console.log(typeof score)
+
+let newScore = Number(score)
+
+console.log(typeof newScore);
+console.log( newScore);
+
+
+// "33" => 33
+//"33a" => NaN
+// true => 1, false => 0
+
+let conversion = 'ABHI'
+
+console.log(conversion)
+
+let newConversion = Boolean(conversion)
+
+console.log(newConversion);
+
+// 1=> true
+//""=> false
+//"Abhi" => true
+
+let str= 33
+console.log(typeof str);
+let newStr = String(str)
+
+console.log(typeof newStr);
+console.log(newStr);
+
+// 33 => "33"
