cb082423a765f471d7f3c3596c8817011f9319ca
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-18 22:49:25 +0000

    Comparision in JS

---
+++ basicJS/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(3 > 4)
+// console.log(3 >= 4)
+// console.log(3 == 4)
+// console.log(3 < 4)
+
+
+//  console.log('3' > 4) //false
+//  console.log('3' >= 4) //false
+//  console.log('3' == 4) //false
+//  console.log('3' < 4) //true
+ 
+//  console.log(null > 0) // false
+//  console.log(null == 0) // false
+//  console.log(null >= 0) // true
+
+
+//  console.log(undefined > 0) // false
+//  console.log(undefined == 0) // false
+//  console.log(undefined >= 0) // false
+
+
+ console.log(true >= 0) // true
+ console.log(true === 1) // false
+ console.log(true == 1) // true
+
+
+ // === strict check not only check value but also the data type
+
+//  console.log('0' === 0) // false
+//  console.log(0 === 0) // true
