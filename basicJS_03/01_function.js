db8e472a3bb31bf56f406dcfe0d1e967c085f6ff
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-24 20:15:42 +0000

    function

---
+++ basicJS_03/01_function.js
@@ -0,0 +1,31 @@
+function printMyName(){
+    console.log('A');
+    console.log('B');
+    console.log('H');
+    console.log('I');
+    console.log('S');
+    console.log('H');
+    console.log('E');
+    console.log('K');
+    
+}
+
+// printMyName()
+
+function addTwoNumber(num1, num2){
+    if(num1 === Number && num2 === Number){
+    return num1 + num2
+    }
+}
+
+
+// console.log(addTwoNumber(1,'a'))
+
+function userNamePrint(username){
+    
+    return(`${username} welcome`)
+    
+}
+
+const abc = userNamePrint('Abhi')
+console.log(abc)
