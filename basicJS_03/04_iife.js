1863efb4cf7ac19a36a6447e4fe161cc89b56998
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-25 20:10:49 +0000

    Minor change

--- basicJS_03/04_iife.js
+++ basicJS_03/04_iife.js
@@ -5,7 +5,7 @@
     console.log(`DB connected Two ${name}`)
 })('Abhi');
 
-//Note special:- we need to provide semicolon otherwise the code excuter beed know where to stop this will give errore without semi colon
+//Note special:- we need to provide semicolon otherwise the code excuter not able know where to stop this will give errore without semi colon
 
 //Avoid polluting the global scope
 //create private varible
