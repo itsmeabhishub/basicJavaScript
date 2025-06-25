22f3b0dc1c69df894e4f3b03189163af95011f2a
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-25 20:10:39 +0000

    IIFE Concept

---
+++ basicJS_03/04_iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    console.log(`DB connected`)
+})();
+((name) =>{
+    console.log(`DB connected Two ${name}`)
+})('Abhi');
+
+//Note special:- we need to provide semicolon otherwise the code excuter beed know where to stop this will give errore without semi colon
+
+//Avoid polluting the global scope
+//create private varible
+//excute code immediately
