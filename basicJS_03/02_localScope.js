793429fffa9bf67bd0f7f38f4135f82965b465aa
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-25 10:30:47 +0000

    minor

--- basicJS_03/02_localScope.js
+++ basicJS_03/02_localScope.js
@@ -1,13 +1,21 @@
+(function(){
+ var d = 10
+})()
+//iife limits the global scope variable to block scope 
+
+
 // Global scope:- var
 // Local scope:- let/const
+
+
 
 {
     let a =10
     const b=20
 }
-
 // not able to fetch out scope
 
 var c = 30
 
 // able to get globally
+console.log(d)
