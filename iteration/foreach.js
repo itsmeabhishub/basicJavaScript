424f89b5d8c74be7568720ab7798c96d500437d0
Abhishek Anand <abhishek.anand8544@gmail.com>
2025-06-27 10:44:51 +0000

    For each

---
+++ iteration/foreach.js
@@ -0,0 +1,21 @@
+const arr = [1,2,3,4,5,6,7,8,9]
+
+//  arr.forEach((item, index, arr) => {console.log(item,index,arr) })
+
+
+ const arrObj = [
+    {
+        'name':'Piyus',
+        'role':"lead developer"
+    },
+    {
+        'name':'Abhishek',
+        'role':"developer"
+    },
+    {
+        'name':'Aryan',
+        'role':"developer"
+    },
+ ]
+
+ arrObj.forEach((item) => console.log(item.role))
