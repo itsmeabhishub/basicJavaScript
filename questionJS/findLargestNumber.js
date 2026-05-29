c569dee074e75c79ca8f3a64c2ea709ee52d9b9b
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-05-29 10:24:06 +0000

    Find Largest Number

--- questionJS/findLargestNumber.js
+++ questionJS/findLargestNumber.js
@@ -1,5 +1,13 @@
 
 
-function findLargestNumber(){
-    
+function findLargestNumber(arr){
+      let l = arr[0]
+      for (let i = 0; i <= arr.length-1; i++){
+        if(arr[i]>l){
+            l=arr[i]
+        }
+      }
+      return l;
 }
+
+console.log(findLargestNumber([121,3,55,77,34,85,200]));
