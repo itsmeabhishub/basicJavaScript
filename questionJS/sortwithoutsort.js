39eec73241933899c00f73c5cfa826469ca2d104
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-05-30 09:43:03 +0000

    sorting without method

---
+++ questionJS/sortwithoutsort.js
@@ -0,0 +1,18 @@
+
+
+function sort(arr){
+    let newArr= []
+    for(i=0; i<= arr.length -1; i++){
+        for(j=0; j<=arr.length -1 -i; j++){
+            if(arr[j]>arr[j+1] ){
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1]= temp
+            }
+        }
+    }
+    return arr;
+
+}
+
+console.log(sort([23,1,43,22,56,35]));
