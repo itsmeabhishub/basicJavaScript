ced9b85c9d49992cb43210907febf76a4c2679b6
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-05-31 06:22:45 +0000

    Zero at end of array

---
+++ questionJS/zero.js
@@ -0,0 +1,17 @@
+
+
+function moveZero(arr){
+    let pos = 0
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== 0){
+            let temp = arr[pos]
+            arr[pos]= arr[i]
+            arr[i] = temp
+
+            pos++
+        }
+    }
+    return arr
+}
+
+console.log(moveZero([0,1,0,2,0,3,4,5,0]))
