8b3ae6b9f1e1475cc8f7cac6010d13336cab516c
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-01 13:32:17 +0000

    Longest word in sentence

---
+++ questionJS/longestWord.js
@@ -0,0 +1,18 @@
+
+
+
+function longestWrd(str){
+    let word = str.split(" ")
+    let longest = ""
+
+    for (let i = 0; i<word.length; i++){
+        if(word[i].length > longest.length){
+            longest= word[i]
+        }
+    }
+    return longest
+}
+
+let word= "I love javascript programming"
+
+console.log(longestWrd(word));
