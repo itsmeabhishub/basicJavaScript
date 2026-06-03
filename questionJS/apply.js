3399d694f9232714b4953830a52174d0efa585f2
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-03 20:47:30 +0000

    Apply

---
+++ questionJS/apply.js
@@ -0,0 +1,29 @@
+
+function greet(city,state){
+    console.log(this.name , city, state);
+    
+}
+
+const person1={
+    name:'Abhishek'
+}
+
+greet.apply(person1, ["Jamui", "Bihar"])
+
+
+function myApply(fn,obj, arg=[]){
+         obj.temp = fn
+
+        const result= obj.temp(...arg)
+
+        delete obj.temp
+
+        return result
+}
+
+function greet1( city, state){
+    console.log(this.name, city, state);
+    
+}
+
+myApply(greet,person1,['Jamui', "bihar"])
