83a42720c768dcef2e5bebda7af3db7ba668c6ad
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-03 19:44:44 +0000

    custom reduce

---
+++ questionJS/customReduce.js
@@ -0,0 +1,12 @@
+function myReduce(arr, callback, intialValue){
+let ans= intialValue
+    for(let i=0; i<arr.length; i++){
+        ans=callback(ans,arr[i], i, arr)
+    }
+    return ans
+}
+
+const arr1=[1,2,3,4]
+const reduce= myReduce(arr1, (acc, red)=> acc+red,0)
+
+console.log(reduce);
