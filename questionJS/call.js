9b6615842890ce0f00ed55c50f65a847f9f3d60a
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-03 20:25:29 +0000

    Call

---
+++ questionJS/call.js
@@ -0,0 +1,30 @@
+const person1 ={
+    name:"Abhishek"
+}
+
+function greet(){
+    console.log(this.name);
+    
+}
+
+greet.call(person1)
+
+function myCall(fn, object, ...args){
+
+    object.temp = fn;
+     const result = object.temp(...args);
+
+     delete object.temp
+     return result
+
+}
+
+function greet1(city){
+    console.log(this.name, city);
+}
+
+const user={
+    name:"Abhishek"
+}
+
+myCall(greet1, user, "Jamui")
