a805315a95e6a5a38aa3b2c85315cfd00c03d82a
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-15 17:48:25 +0000

    Revision

--- questionJS/armstrongNumber.js
+++ questionJS/armstrongNumber.js
@@ -1,22 +1,22 @@
 
 
-function armStrng(int){
-    let mainNum= int;
+function armStrng(int) {
+    let mainNum = int;
     let res = 0;
     let digits = String(int).length
-    while(int>0){
-        let num=int%10;
+    while (int > 0) {
+        let num = int % 10;
         res += num ** digits;
-        int = Math.floor(int/10)
+        int = Math.floor(int / 10)
     }
     console.log(res);
     console.log(mainNum);
-    
+
     if (res === mainNum) {
         console.log("Number is armstrong")
-    }else{
+    } else {
         console.log("Number is not armstrong")
     }
-    
+
 }
 armStrng(154)
