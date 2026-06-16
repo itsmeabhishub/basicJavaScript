60e3365b8a527ad449c45bba9aea814fc273a839
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-16 18:21:41 +0000

    Revision

--- questionJS/call.js
+++ questionJS/call.js
@@ -1,30 +1,30 @@
-const person1 ={
-    name:"Abhishek"
+const person1 = {
+    name: "Abhishek"
 }
 
-function greet(){
+function greet() {
     console.log(this.name);
-    
+
 }
 
 greet.call(person1)
 
-function myCall(fn, object, ...args){
+function myCall(fn, object, ...args) {
 
     object.temp = fn;
-     const result = object.temp(...args);
+    const result = object.temp(...args);
 
-     delete object.temp
-     return result
+    delete object.temp
+    return result
 
 }
 
-function greet1(city){
+function greet1(city) {
     console.log(this.name, city);
 }
 
-const user={
-    name:"Abhishek"
+const user = {
+    name: "Abhishek"
 }
 
 myCall(greet1, user, "Jamui")
