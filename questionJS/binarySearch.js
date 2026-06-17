33a160aa5780107d9791e19079e6c992bc379aed
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-17 17:08:31 +0000

    Revision

--- questionJS/binarySearch.js
+++ questionJS/binarySearch.js
@@ -1,23 +1,23 @@
 
 
-function binarySearch(arr){
+function binarySearch(arr) {
 
-    target =14
-    let start= 0;
-    let end = arr.length-1
+    target = 14
+    let start = 0;
+    let end = arr.length - 1
 
-    while(start <= end){
-        let mid = Math.floor((start+end)/2)
-        if(arr[mid]=== target){
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2)
+        if (arr[mid] === target) {
             return mid
         }
-        if(arr[mid]< target){
-            start = mid +1;
-        }else{
+        if (arr[mid] < target) {
+            start = mid + 1;
+        } else {
             end = mid - 1
         }
     }
     return -1
 }
 
-console.log(binarySearch([2,4,6,8,10,12,14,16,18]));
+console.log(binarySearch([2, 4, 6, 8, 10, 12, 14, 16, 18]));
