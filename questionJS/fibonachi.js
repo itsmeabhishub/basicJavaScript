5e6b006b4dc7db4b2f6456668639cea668772acf
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-18 13:29:46 +0000

    Revision

--- questionJS/fibonachi.js
+++ questionJS/fibonachi.js
@@ -1,15 +1,15 @@
 
 
-function fiboSeries(int){
-  let a= 0;
-  let b=1
+function fiboSeries(int) {
+  let a = 0;
+  let b = 1
   let fabo
-  if(int === 0) return 0;
-  if(int === 0) return 1;
-  for(let i=2; i<=int; i++){
-     fabo = a+b;
-    a=b;
-    b=fabo
+  if (int === 0) return 0;
+  if (int === 0) return 1;
+  for (let i = 2; i <= int; i++) {
+    fabo = a + b;
+    a = b;
+    b = fabo
   }
   return b;
 }
