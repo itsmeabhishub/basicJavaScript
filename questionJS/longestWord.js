c56b6fa54b4285f419ba2d201ba7e3a4413bac17
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-20 16:48:56 +0000

    Revision

--- questionJS/longestWord.js
+++ questionJS/longestWord.js
@@ -1,18 +1,18 @@
 
 
 
-function longestWrd(str){
+function longestWrd(str) {
     let word = str.split(" ")
     let longest = ""
 
-    for (let i = 0; i<word.length; i++){
-        if(word[i].length > longest.length){
-            longest= word[i]
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].length > longest.length) {
+            longest = word[i]
         }
     }
     return longest
 }
 
-let word= "I love javascript programming"
+let word = "I love javascript programming"
 
 console.log(longestWrd(word));
