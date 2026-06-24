372f3c8927a3873b0bc53d17c0bfa903a5fad1f9
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-24 18:49:53 +0000

    Revision

--- questionJS/Revesion.js
+++ questionJS/Revesion.js
@@ -1,18 +1,17 @@
 
-function reverseString(int){
+function reverseString(int) {
     let res = 0;
-    while(int>0){
-        let num= int %10;
-        res = res*10 + num;
-        int= Math.floor(int/10)
+    while (int > 0) {
+        let num = int % 10;
+        res = res * 10 + num;
+        int = Math.floor(int / 10)
     }
     return res;
 }
 
 console.log(reverseString(1211));
 
-function reverseFunc(num){
+function reverseFunc(num) {
     return Number(num.toString().split("").reverse().join(""))
 }
 console.log(reverseFunc(321));
- 
