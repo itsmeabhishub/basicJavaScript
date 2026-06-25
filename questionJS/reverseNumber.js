f42ec6d9d5537de7e9cdac92e67e0f0473617eaa
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-25 17:34:40 +0000

    Revision

--- questionJS/reverseNumber.js
+++ questionJS/reverseNumber.js
@@ -1,17 +1,17 @@
 
 
-function revfunc(num){
+function revfunc(num) {
     return Number(num.toString().split("").reverse().join(""))
 }
 console.log(revfunc(12345));
 
 
-function revfuncLoop(num){
+function revfuncLoop(num) {
     let n = 0
-    while(num>0){
-            let rem = num%10;
-            n = n*10 + rem;
-            num= Math.floor(num/10)
+    while (num > 0) {
+        let rem = num % 10;
+        n = n * 10 + rem;
+        num = Math.floor(num / 10)
     }
     return n;
 }
