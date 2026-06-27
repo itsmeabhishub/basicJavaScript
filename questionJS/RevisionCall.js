7cd377096ce5336956aa109ade1f34bd56b16f30
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-27 18:19:48 +0000

    Revision

--- questionJS/RevisionCall.js
+++ questionJS/RevisionCall.js
@@ -1,21 +1,20 @@
 
 
 function customCall(f1, object, ...arg) {
-    object.temp= f1
+    object.temp = f1
 
     const result = object.temp(...arg)
     delete object.temp
-
     return result
 }
 
 
-function greet(city){
+function greet(city) {
     console.log(this.name, city)
 }
 
-const person={
-    name:"Abhishek"
+const person = {
+    name: "Abhishek"
 }
 
-customCall(greet,person,'Jamui')
+customCall(greet, person, 'Jamui')
