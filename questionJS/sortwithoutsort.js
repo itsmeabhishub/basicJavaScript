1aca9cd07d66a030a956e4f1421b6adbb8513fd4
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-06-30 19:06:12 +0000

    Revision

--- questionJS/sortwithoutsort.js
+++ questionJS/sortwithoutsort.js
@@ -1,13 +1,13 @@
 
 
-function sort(arr){
-    
-    for(i=0; i<= arr.length -1; i++){
-        for(j=0; j<=arr.length -1 -i; j++){
-            if(arr[j]>arr[j+1] ){
+function sort(arr) {
+
+    for (i = 0; i <= arr.length - 1; i++) {
+        for (j = 0; j <= arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
                 let temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1]= temp
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
             }
         }
     }
@@ -17,4 +17,4 @@ function sort(arr){
 
 }
 
-console.log(sort([23,1,43,22,56,35]));
+console.log(sort([23, 1, 43, 22, 56, 35]));
