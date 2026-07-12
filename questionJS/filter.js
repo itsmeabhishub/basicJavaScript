a7755b6993ce3bb2b6abcaadc0db5b4e08106ef4
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-07-12 18:02:45 +0000

    Revision

--- questionJS/filter.js
+++ questionJS/filter.js
@@ -1,25 +1,24 @@
 
 
-const arr=[2,3,4,5]
+const arr = [2, 3, 4, 5]
 
-const result = arr.filter((i)=>
-    { return i>3})
+const result = arr.filter((i) => { return i > 3 })
 
 console.log(result)
 
 
-function myFilter(int, callback){
+function myFilter(int, callback) {
     let res = []
-        for(let i =0; i<int.length; i++){
-           if(callback(arr[i],i, arr)){
+    for (let i = 0; i < int.length; i++) {
+        if (callback(arr[i], i, arr)) {
             res.push(arr[i])
-           }
         }
-        return res
+    }
+    return res
 }
 
-const arr1 = [1,2,3,4]
+const arr1 = [1, 2, 3, 4]
 
-const abc= myFilter(arr1, (i)=> {return i>2})
+const abc = myFilter(arr1, (i) => { return i > 2 })
 
 console.log(abc);
