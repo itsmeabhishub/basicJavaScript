0f8d55455de1409e7bb350155de4037c05ce6509
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-07-16 18:43:31 +0000

    Revision

--- questionJS/characterOccurance.js
+++ questionJS/characterOccurance.js
@@ -1,17 +1,17 @@
 
 
 
-function charOccurance(char){
-    let obj={}
-    for(let i = 0; i<char.length; i++){
-       let ch= char[i]
-        if(obj[ch]){
+function charOccurance(char) {
+    let obj = {}
+    for (let i = 0; i < char.length; i++) {
+        let ch = char[i]
+        if (obj[ch]) {
             obj[ch]++;
-        }else{
-            obj[ch]=1;
+        } else {
+            obj[ch] = 1;
         }
-}
-return obj
+    }
+    return obj
 }
 
 console.log(charOccurance('Abhishek anand'))
