f59aeb392d5a5670b20d00d175d94e77d6a2d525
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-07-17 18:33:15 +0000

    Revision

--- questionJS/commonElement.js
+++ questionJS/commonElement.js
@@ -1,10 +1,10 @@
 
 
-function commonEle(arr1,arr2){
-    let CommonElement=[]
-    for(let i=0; i<=arr1.length-1; i++){
-        for(let j=0; j<=arr2.length-1; j++){
-            if(arr1[i]===arr2[j]){
+function commonEle(arr1, arr2) {
+    let CommonElement = []
+    for (let i = 0; i <= arr1.length - 1; i++) {
+        for (let j = 0; j <= arr2.length - 1; j++) {
+            if (arr1[i] === arr2[j]) {
                 CommonElement.push(arr2[j])
             }
         }
@@ -12,4 +12,4 @@ function commonEle(arr1,arr2){
     return CommonElement
 }
 
-console.log(commonEle([10,20,30,40,6],[30,40,50,60]));
+console.log(commonEle([10, 20, 30, 40, 6], [30, 40, 50, 60]));
