e78ddc0c1eba2986326ea3eed1189500297d021b
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-07-18 18:17:04 +0000

    Revision

--- questionJS/compressString.js
+++ questionJS/compressString.js
@@ -1,14 +1,14 @@
 
 
-function compresstr(str){
+function compresstr(str) {
     let count = 1
     let res = ''
-    for(let i = 0; i<str.length; i++){
-        if(str[i]===str[i+1]){
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
             count++
-        }else{
-            res+= str[i] + count;
-            count =1
+        } else {
+            res += str[i] + count;
+            count = 1
         }
     }
     return res
