8aeccc2a53011266341b20a80ccefb7fe563fe13
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-08-04 16:48:45 +0000

    Revision

--- questionJS/firstNonRep.js
+++ questionJS/firstNonRep.js
@@ -1,22 +1,22 @@
 
 
-function nonRepat(str){
-    str= str.toLowerCase()
-    for(i=0; i<str.length; i++){
-        let isDuplicate= false
-        for(j=i+1; j<str.length; j++){
-            if(str[i] === str[j]){
+function nonRepat(str) {
+    str = str.toLowerCase()
+    for (i = 0; i < str.length; i++) {
+        let isDuplicate = false
+        for (j = i + 1; j < str.length; j++) {
+            if (str[i] === str[j]) {
                 isDuplicate = true
             }
         }
         let isRepeat = false
-        for(let k= 0; k<i; k++){
-                if(str[k] === str [i]){
-                    isRepeat =true
-                }
+        for (let k = 0; k < i; k++) {
+            if (str[k] === str[i]) {
+                isRepeat = true
+            }
         }
-        
-        if(!isDuplicate && !isRepeat){
+
+        if (!isDuplicate && !isRepeat) {
             console.log(str[i]);
             return
         }
