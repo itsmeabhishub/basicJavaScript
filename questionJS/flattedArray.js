7cdf96e33bc3b697a005ae2df6506f1a069be38c
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-08-05 17:42:37 +0000

    Revision

--- questionJS/flattedArray.js
+++ questionJS/flattedArray.js
@@ -1,17 +1,17 @@
 
-let flarArr=[]
-function flattenArray(arr){
-    
-  for(let i =0; i<= arr.length-1;i++){
-    if(Array.isArray(arr[i])){
-        flattenArray(arr[i])
-    }else{
-        flarArr.push(arr[i])
+let flarArr = []
+function flattenArray(arr) {
+
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (Array.isArray(arr[i])) {
+      flattenArray(arr[i])
+    } else {
+      flarArr.push(arr[i])
     }
   }
-  
+
   return flarArr;
-return arr.flat(Infinity)
+  return arr.flat(Infinity)
 }
 
-console.log(flattenArray([1,2,3,4,5,[33,44,55,[65,69,87]]]));
+console.log(flattenArray([1, 2, 3, 4, 5, [33, 44, 55, [65, 69, 87]]]));
