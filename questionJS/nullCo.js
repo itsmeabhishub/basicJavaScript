d2fbafa068ae1ceb93ce740cf6982e226b12ef3a
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-08-11 18:26:42 +0000

    Revision

--- questionJS/nullCo.js
+++ questionJS/nullCo.js
@@ -1,10 +1,10 @@
 
 
 const obj = {
-        name:'Abhishek',
-        address:{
-            city:'jamui'
-        }
+    name: 'Abhishek',
+    address: {
+        city: 'jamui'
+    }
 }
 
 console.log(obj?.address?.city ?? 'something went wrong')
