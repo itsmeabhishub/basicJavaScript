8187a3e17df00a45878cdba218891e26f29419f9
Abhishek Anand <abhishek.anand8544@gmail.com>
2026-08-21 16:45:49 +0000

    Revision

--- questionJS/RevesionCustombind.js
+++ questionJS/RevesionCustombind.js
@@ -1,21 +1,21 @@
 
 
-function customBind(f1, context, ...arg){
+function customBind(f1, context, ...arg) {
 
-    return function(...runtimeArg){
+    return function (...runtimeArg) {
         return f1.call(context, ...arg, ...runtimeArg)
-    } 
+    }
 }
 
-function greet(city, state){
-    console.log(this.name,city, state);
-    
+function greet(city, state) {
+    console.log(this.name, city, state);
+
 }
 
-const people={
-    name:"Abhishek"
+const people = {
+    name: "Abhishek"
 }
 
-const bounFn= customBind(greet,people,"Jamui")
+const bounFn = customBind(greet, people, "Jamui")
 
 bounFn('Bihar')
